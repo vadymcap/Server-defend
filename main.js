0762948ab9c93f88c3b2f8e3f291cff8715a9eaf
Cap <whataboutcap@gmail.com>
2026-03-21 12:05:35 +0200

    Proton

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
